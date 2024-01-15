37809a463ca3cc3b0c76b21024996e6e5365504c
David Vazquez <dave_u@outlook.com>
2024-01-14 21:47:25 -0600

    terminado

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPreesed(_ sender: UIButton) {
+        print("Start")
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2 ) {
+            sender.alpha = 1.0
+            print("End")
+        }
+        playSound( soundName: sender.currentTitle! )
+        
+        
+    }
     
-
+    func playSound( soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
